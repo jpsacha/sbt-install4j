e721332707b1f95f51dd6e00042092e9eb99feee
Jarek Sacha <jpsacha@gmail.com>
2014-01-23 22:28:40 -0500

    Initial working version of the install4j plugin.

---
+++ build.sbt
@@ -0,0 +1,54 @@
+sbtPlugin := true
+
+name := "sbt-install4j"
+
+organization := "net.sf.ij-plugins"
+
+version := "0.0.1-SNAPSHOT"
+
+description := "SBT plugin that helps create runtime directory structure for ImageJ plugin development."
+
+homepage := Some(url("http://github.com/jpsacha/sbt-install4j"))
+
+organizationHomepage := Some(url("http://ij-plugins.sf.net"))
+
+startYear := Some(2014)
+
+licenses := Seq("GPLv3" -> url("http://www.gnu.org/licenses/gpl.html"))
+
+scalacOptions := Seq("-deprecation", "-unchecked")
+
+publishArtifact in(Compile, packageBin) := true
+
+publishArtifact in(Test, packageBin) := false
+
+publishArtifact in(Compile, packageDoc) := true
+
+publishArtifact in(Compile, packageSrc) := true
+
+publishArtifact in(Test, packageDoc) := false
+
+publishArtifact in(Test, packageSrc) := false
+
+//publishMavenStyle := false
+
+publishTo <<= version {
+  version: String =>
+    if (version.contains("-SNAPSHOT"))
+      Some("Sonatype Nexus Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots")
+    else
+      Some("Sonatype Nexus Releases" at "https://oss.sonatype.org/service/local/staging/deploy/maven2")
+}
+
+pomExtra :=
+  <scm>
+    <url>git@github.com:jpsacha/sbt-imagej.git</url>
+    <connection>scm:git@github.com:jpsacha/sbt-install4j.git</connection>
+  </scm>
+    <developers>
+      <developer>
+        <id>jpsacha</id>
+        <name>Jarek Sacha</name>
+        <url>https://github.com/jpsacha</url>
+      </developer>
+    </developers>
---
+++ src/main/scala/net/ij_plugins/sf/sbt/install4j/Plugin.scala
@@ -0,0 +1,81 @@
+/*
+ * Copyright (C) 2014 Jarek Sacha
+ * email: jpsacha at gmail dot com
+ *
+ * This file is part of sbt-install4j.
+ */
+
+package net.ij_plugins.sf.sbt.install4j
+
+import sbt._
+import sbt.Keys._
+import java.io.{IOException, File}
+
+/** SBT plugin that helps create runtime directory structure for ImageJ plugin development. */
+object Plugin extends sbt.Plugin {
+
+  import Install4JKeys._
+
+  object Install4JKeys {
+    lazy val install4j = TaskKey[Unit]("install4j", "Builds Install4J project.")
+
+    lazy val install4jHomeDir = SettingKey[File]("install4jHomeDir",
+      "Install4J installation directory. It assumes that Install4J compiler is in subdirectory `bin/install4jc.exe`")
+
+    lazy val install4jProjectFile = SettingKey[String]("install4jProjectFile",
+      "The install4j project file that should be build.")
+
+    lazy val install4jVerbose = SettingKey[Boolean]("install4jVerbose","Enables verbose mode.")
+  }
+
+  lazy val install4jSettings: Seq[Def.Setting[_]] = Seq(
+
+    install4j := {
+
+      // Depends on `packageBin`
+      val p = (packageBin in Compile).value
+
+      val install4jCompiler = new File(install4jHomeDir.value, "bin/install4jc.exe").getCanonicalFile
+      val install4jProject = new File(baseDirectory.value, install4jProjectFile.value).getCanonicalFile
+
+      runInstall4J(
+        install4jCompiler,
+        install4jProject,
+        verbose = install4jVerbose.value,
+        streams.value)
+    }, //.dependsOn(packageBin in Compile),
+
+    install4jHomeDir := file("C:/Program Files/install4j5"),
+
+    install4jProjectFile := "installer/installer.install4j",
+
+    install4jVerbose := false
+  )
+
+  private def log(msg: String) {
+    println("[sbt-install4j] " + msg)
+  }
+
+  private def runInstall4J(compiler: File,
+                           project: File,
+                           verbose: Boolean,
+                           taskStreams: TaskStreams) {
+    val logger = taskStreams.log
+
+    logger.debug("install4jCompiler: " + compiler.getAbsolutePath)
+    if (!compiler.exists) throw new IOException("Install4J Compiler not found: " + compiler.getAbsolutePath)
+
+    log("project: " + project.getAbsolutePath)
+    if (!project.exists) {
+      throw new IOException("install4j project file not found: " + project.getAbsolutePath)
+    }
+
+    var commandLine = "\"" + compiler.getPath + "\""
+    if (verbose) commandLine += " --verbose "
+    commandLine += "\"" + project.getPath + "\""
+
+    log("Executing command: " + commandLine)
+    val output = Process(commandLine).lines
+    output.foreach(println)
+  }
+}
