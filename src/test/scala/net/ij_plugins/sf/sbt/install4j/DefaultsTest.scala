36d08caefbfbe3b30f993a10cba2ade59727f07e
Jarek Sacha <jpsacha@gmail.com>
2022-02-07 20:54:21 -0500

    Update new API in ScalaTest

--- src/test/scala/net/ij_plugins/sf/sbt/install4j/DefaultsTest.scala
+++ src/test/scala/net/ij_plugins/sf/sbt/install4j/DefaultsTest.scala
@@ -1,14 +1,30 @@
+/*
+ * Copyright 2014-2022 Jarek Sacha (jpsacha -at- gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package net.ij_plugins.sf.sbt.install4j
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers.*
 
 import java.io.File
 
-import org.scalatest.FlatSpec
-import org.scalatest.Matchers._
-
-class DefaultsTest extends FlatSpec {
+class DefaultsTest extends AnyFlatSpec {
 
   it should "determine install4jHomeDir from environment variable" in {
-    val f = File.createTempFile("_env_", "_env_")
+    val f         = File.createTempFile("_env_", "_env_")
     val path_name = f.getCanonicalPath
     System.setProperty(Defaults.INSTALL4J_HOME_ENV, path_name)
 
@@ -18,7 +34,7 @@ class DefaultsTest extends FlatSpec {
   }
 
   it should "determine install4jCompilerFile from environment variable" in {
-    val f = File.createTempFile("_env_", "_env_")
+    val f         = File.createTempFile("_env_", "_env_")
     val path_name = f.getCanonicalPath
     System.setProperty(Defaults.INSTALL4JC_FILE_ENV, path_name)
 
